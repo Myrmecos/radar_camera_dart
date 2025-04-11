5dc8db2cc45f653450b54f6e59a5b77081fba24c
Myrmecos <etmphile@connect.hku.hk>
2025-04-11 16:34:37 +0800

    during testing, can target a green dot

--- src/img_sub/img_sub/image_sub.py
+++ src/img_sub/img_sub/image_sub.py
@@ -1,9 +1,27 @@
 import rclpy
 import cv2 as cv
+import numpy as np
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+from .imageprocessor import GLPosition
+from ament_index_python.packages import get_package_share_directory
+import yaml
+import os
 
+def drawline(image, pos, direction):
+    thickness = 10
+    color = (255, 0, 0)
+
+    if direction==0: #horizontals
+        start_point = (pos, 0)
+        end_point = (pos, image.shape[1])
+        cv.line(image, start_point, end_point, color, thickness)
+
+    if direction==1:
+        start_point (0, pos)
+        end_point = (image.shape[0], pos)
+        cv.line(image, start_point, end_point, color, thickness)
 
 class MinimalSubscriber(Node):
 
@@ -16,15 +34,45 @@ class MinimalSubscriber(Node):
             10)
         self.subscription  # prevent unused variable warning
         self.bridge = CvBridge() #initialize openCV bridge
+       
+        yaml_path = "/home/astar/ros_ws/src/img_sub/config/cameraparam.yaml"
+        self.glp = GLPosition(camera_param_path=yaml_path)
+#image = cv.imread("img_dir/0.png")
+# glp = GLPosition()
+# pos = glp.find_green_light(image)
+# #rel_pos = glp.pos_relative_to_center(pos)
+# pos = (1024, 1280)
+# print("relative position to center: ", pos)
+# glp.get_camera_intrinsic_distortion("camparam.yaml")
+# print("distortion coefficient: \n", glp.distort)
+# print("intrinsic matrix: \n", glp.IM)
+# print("angle relative to camera center: ", glp.get_GL_angle_relative(rel_pos))
+
+
 
 
     def listener_callback(self, msg):
         try:
-            cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding = 'bgr8')
-            cv.imshow("image", cv_image)
+            image = self.bridge.imgmsg_to_cv2(msg, desired_encoding = 'bgr8')
+            image = cv.imread("/home/astar/ros_ws/src/img_sub/config/14.png")
+            self.get_logger().error("checkpoint1")
+            # process image
+            pos = self.glp.find_green_light(image)
+            self.get_logger().error("checkpoint2")
+            self.get_logger().error(f"relative green light position to center:{pos}")
+
+            if pos:
+                angles = self.glp.get_GL_angle_relative(pos)
+                print("angle relative to camera direction: (yaw, pitch)", angles) #rotation along xy plane, rotation at yz plane
+                drawline(image, np.round(pos[0]), 0)
+                drawline(image, np.round(pos[1]), 1)
+
+            cv.imshow("image", image)
             cv.waitKey(10)
         except Exception as e:
             self.get_logger().error(f"error processing image: {str(e)}")
+            import traceback
+            traceback.print_stack(e)
         
 
 
---
+++ src/img_sub/img_sub/imageprocessor.py
@@ -0,0 +1,108 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+import yaml
+'''
+Green Light Position Determination
+A class to determine the relative position between camera and green light
+'''
+class GLPosition():
+
+    '''
+    image_widht and image_height is the width and height of images
+    taken by our camera.
+    The width and height will be used to determine the relative angle between camera and green light.
+    camera_param_path is the path to yaml file containing camera intrinsic matrix and distortion coefficient
+    '''
+    def __init__(self, image_width=1280, image_height=1024, camera_param_path = "camparam.yaml", lower_color = [240, 240, 0], upper_color = [255, 255, 150]):
+        
+        
+        self.image_width = image_width
+        self.image_height = image_height
+        self.get_camera_intrinsic_distortion(camera_param_path)
+        self.lower_color = np.array(lower_color)
+        self.upper_color = np.array(upper_color)
+
+    '''
+    task: load camera intrinsic from a yaml file
+    '''
+    def get_camera_intrinsic_distortion(self, yaml_file_name):
+        with open(yaml_file_name, 'r') as file:
+            contents = yaml.safe_load(file)
+    
+        self.IM = np.matrix(contents["intrinsic_matrix"])
+        self.distort = np.matrix(contents["distortion_coefficient"])
+
+    '''
+    Task: given the image that contains a green dot
+    report its center coordinate in the image
+    input: image
+    output, tuple (x, y). x and y can be float
+    '''
+    def find_green_light(self, image):
+        mask = cv.inRange(image, self.lower_color, self.upper_color)
+        plt.imshow(mask)
+        #plt.show()
+        #return
+        contours, _ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        if contours: 
+            largest_contour = max(contours, key=cv.contourArea)
+            M = cv.moments(largest_contour)
+            if M["m00"] !=0:
+                cx = M["m10"]/M["m00"]
+                cy = M["m01"]/M["m00"]
+                center = (cx, cy)
+            else:
+                center = None
+        else: 
+            print("found none")
+            center=None
+        return center
+    '''
+    Task: given a position in the image
+    return its position (coordinate) relative to center
+    input: pos = [x_coordinate, y_coordinate]
+    output: tuple (x, y). x and y can be float
+    '''
+    def pos_relative_to_center(self, pos):
+        relative_x = pos[0] - self.image_width/2
+        relative_y = pos[1] - self.image_height/2
+        return (relative_x, relative_y)
+
+    '''
+    Task: given a pixel position to image center
+    calculate the angle differnence between camera direction and green light
+    input: relative position of light to camera, 2-tuple (x_dev_from_center, y_dev_from_center)
+    output: relative position of light to camera, 2-tuple (x_angle_from_center, y_angle_from_center)
+    '''
+    def get_GL_angle_relative(self, pixel_pos):
+        # first, prepare the pixel coordinate
+        x, y = pixel_pos
+        x, y = 639, 511
+        pts = np.array([[[x, y]]], dtype=np.float32)
+
+        print("position relative to center before un-distortion:", pts[0][0])
+        # then, undistort and normalize coordinates
+        undistorted_pts = cv.undistortPoints(pts, self.IM, self.distort)
+        print("position of circle center after un-distortion:", undistorted_pts[0][0])
+        x_norm, y_norm = undistorted_pts[0][0]
+
+        # finally, get the tangent value of each side
+        angle_x = np.arctan2(x_norm, 1)
+        angle_y = np.arctan2(y_norm, 1)
+
+        return (angle_x, angle_y)
+
+
+
+
+# image = cv.imread("img_dir/0.png")
+# glp = GLPosition()
+# pos = glp.find_green_light(image)
+# #rel_pos = glp.pos_relative_to_center(pos)
+# pos = (1024, 1280)
+# print("relative position to center: ", pos)
+# glp.get_camera_intrinsic_distortion("camparam.yaml")
+# print("distortion coefficient: \n", glp.distort)
+# print("intrinsic matrix: \n", glp.IM)
+# print("angle relative to camera center: ", glp.get_GL_angle_relative(rel_pos))
